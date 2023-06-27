998ba0963e54ae41e2df3e3113e695d0ddb9393e
nkarl <21993921+nkarl@users.noreply.github.com>
2023-06-27 08:46:51 -0700

    Added first implementation of handling request and selective responding.

--- src/main.rs
+++ src/main.rs
@@ -18,11 +18,11 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
 
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    //let http_request: Vec<_> = buf_reader
+    //.lines()
+    //.map(|result| result.unwrap())
+    //.take_while(|line| !line.is_empty())
+    //.collect();
 
     /*
      * Request Form:
@@ -32,18 +32,33 @@ fn handle_connection(mut stream: TcpStream) {
      *
      * NOTE: GET request has no body
      */
-    println!("Request: {:#?}", http_request);
+    //println!("Request: {:#?}", http_request);
 
     /*
-     * Response Form:
-     *      HTTP-Version Status-Code Reason-Phrase CRLF
-     *      headers CRLF
-     *      message-body
+     * Validating the request and Selectively responding
      */
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    stream.write_all(response.as_bytes()).unwrap();
+    if request_line == "GET / HTTP/1.1" {
+        /*
+         * Response Form:
+         *      HTTP-Version Status-Code Reason-Phrase CRLF
+         *      headers CRLF
+         *      message-body
+         */
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
